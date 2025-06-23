cf767563df0fc6cfde1d331b16fd38b914826688
qiy2019 <qiy2019@outlook.com>
2025-06-23 14:43:06 -0700

    After watching the video, added 3 other functions (one for each extra question)

--- Qi_Java_Projects/Qi_SumNumbers/QSumNumbers.java
+++ Qi_Java_Projects/Qi_SumNumbers/QSumNumbers.java
@@ -1,44 +1,67 @@
 package Qi_SumNumbers;
 
 public class QSumNumbers {
+    /*
+     * Prints out what was returned from the variations of sum()
+     *
+     * @param Comand-line arguments. Not used here.
+     */
+    public static void main(String[] args) {
+        System.out.println("The sum of numbers to 3 is " + sum(3));
+        System.out.println("The sum of numbers from 2 to 5 is " + sum(2, 5));
+        System.out.println("The sum of numbers from 3 to 11, increasing by 2 each time, is " + sum(3,11,2));
+        System.out.println("The sum of numbers from 2.4 to 3.8, increasing by 0.2 each time, is " + sum(2.4, 3.8, 0.2));
+    }
 
     /*
-     * Sums up numbers from 1 to 10 inclusive, like, 1+2+3+4+5+6+7+8+9+10
+     * Sums up numbers from 1 to given number inclusive. If number was 10, it'll calculate 1+2+3+4+5+6+7+8+9+10
      *
      * @param No arguments
      * @return the sum of numbers from 1 to 10
      */
     public static int sum(int n) {
-        /*
-         * Define the variable: currSum (current sum)
-         *
-         * What if we want to give a starting value as well?
-         * Set this variable to another number to change starting value
-         * Example: if you want to start at 5, set currSum to 5
-         */
+        // Define the variable: currSum (current sum)
         int currSum = 0; //
 
         // Repeat 10 times
         for (int i = 1; i <= n; i++) {
-            /*
-             * Add the current number to the sum
-             *
-             * What if we want to increment by a specific value instead of one?
-             * Add desired value to i (in the for loop statement)
-             * Example: If the desired value is 2, for (int i = 1; i <= n; i+=2)
-             */
+            // Add the current number to the sum
             currSum += i;
         }
-
         return currSum;
     }
 
-    /*
-     * Prints out what was returned from sum()
-     *
-     * @param Comand-line arguments. Not used here.
-     */
-    public static void main(String[] args) {
-        System.out.println(sum(10));
+
+    public static int sum(int start, int end) {
+        // Define the variable: currSum
+        int currSum = 0;
+
+        // Set i = start so that in the first iteration, i starts at the desired value
+        for (int i = start; i <= end; i++) {
+            currSum += i;
+        }
+        return currSum;
+    }
+
+    public static int sum(int start, int end, int incrBy) {
+        // Define the variable: currSum
+        int currSum = 0;
+
+        // Increment by incrBy
+        for (int i = start; i <= end; i+=incrBy) {
+            currSum += i;
+        }
+        return currSum;
+    }
+
+    public static int sum(double start, double end, double incrBy) {
+        // Define the variable currSum as a double.
+        double currSum = 0.0;
+
+        // i should be a double too
+        for (double i = start; i <= end; i+= incrBy) {
+            currSum += i;
+        }
+        return (int) currSum;
     }
 }
