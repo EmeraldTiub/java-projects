56b4679c83a65884cb2aa1b8b1cbb7874e6b35ce
jiny2 <jiny2019@outlook.com>
2025-06-23 13:03:06 -0700

    Payroll.

--- Jin-payroll/JPayroll.java
+++ Jin-payroll/JPayroll.java
@@ -1,5 +1,62 @@
 public class JPayroll {
-    public static void main(String[] args) {
+    /**
+     * Print out the checks.
+     * @return a string.
+     */
+    public static String printChecks() {
+        return "Done printing checks";
+    }
 
+    /**
+     * Calculate the taxes and net pay.
+     * @return a string.
+     */
+    public static String calculateTaxesNetPay() {
+        return "Done calculating taxes and net pay";
+    }
+
+    /**
+     * Calculate the gross pay.
+     * @return a string.
+     */
+    public static String calculateGrossPay() {
+        return "Done calculating gross pay.";
+    }
+    /**
+     * Read the hourly wages.
+     * @return a string.
+     */
+    public static String readHourlyWages() {
+        return "Done reading hourly wages.";
+    }
+
+    /**
+     * Get the weekly hours.
+     * @return a string.
+     */
+    public static String getWeeklyHours() {
+        return "Done getting weekly hours.";
+    }
+
+    /**
+     * Call the other functions
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Retrieve the weekly hours.
+        getWeeklyHours();
+        System.out.println("Retrieved weekly hours.");
+        // Read the hourly wages.
+        readHourlyWages();
+        System.out.println("Read hourly wages.");
+        // Calculate the gross pay.
+        calculateGrossPay();
+        System.out.println("Calculated gross pay.");
+        // Calculate the taxes and net pay.
+        calculateTaxesNetPay();
+        System.out.println("Calculated taxes and net pay.");
+        // Print out the checks.
+        printChecks();
+        System.out.println("Done printing checks.");
     }
 }
