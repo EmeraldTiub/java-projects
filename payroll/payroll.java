33b483d33c38e39f2665f3ff98ce93345fdd21fa
Emerald Tiub <86202719+EmeraldTiub@users.noreply.github.com>
2025-06-22 21:47:50 -0700

    Create payroll.java

---
+++ payroll/payroll.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class Payroll {
+    public static void main(String[] args) {
+        File file = new File("C:/users/jiny2/ideaprojects/java/src/HourlyWages.csv");
+        
+    }
+}
