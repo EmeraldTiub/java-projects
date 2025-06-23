022e716b366763c3984eb95037f1d41a423a30b8
Emerald Tiub <86202719+EmeraldTiub@users.noreply.github.com>
2025-06-22 20:29:46 -0700

    Update DisplayCustomerData.java
    Add comments.

--- displayCustomerData/DisplayCustomerData.java
+++ displayCustomerData/DisplayCustomerData.java
@@ -1,9 +1,12 @@
 public class DisplayCustomerData {
     public static void main(String[] args) {
+        // Make the example customer data.
         String name = "Fred Jones";
         int daysOld = 19436;
         double balance = 567.89;
+        // Convert the age in days to years.
         int yearsOld = (int) daysOld / 365;
+        // Print out the results.
         System.out.println("Name: " + name);
         System.out.println("Age (in years): " + yearsOld);
         System.out.println("Balance: " + balance);
