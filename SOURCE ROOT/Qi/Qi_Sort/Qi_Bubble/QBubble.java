5fd4da7a115c7a9acddbde681cedc513db44fbc9
qiy2019 <qiy2019@outlook.com>
2025-06-29 12:58:44 -0700

    [ Bubble Sort ] My initial code with Optimization #1 and #2 + additional initial idea content for the optimization part of the demo

--- SOURCE ROOT/Qi/Qi_Sort/Qi_Bubble/QBubble.java
+++ SOURCE ROOT/Qi/Qi_Sort/Qi_Bubble/QBubble.java
@@ -8,10 +8,14 @@ public class QBubble {
      * @return  The sorted version of nums
      */
     public static int[] bubbleSort(int[] nums) {
+        int sortedIdx = nums.length - 1; // Optimization #2
+
         for (int i : nums) { // The maximum number of passes is the length of nums
 
+            boolean hasSwapped = false; // Optimization #1
+
             // Go through everything except for the last item (more explanation in "initial idea")
-            for (int j = 0; j < nums.length - 1; j++) {
+            for (int j = 0; j < sortedIdx; j++) {
 
                 // If nums[j] and nums[j+1] need swapping, do it.
                 if (nums[j] > nums[j+1]) {
@@ -20,8 +24,17 @@ public class QBubble {
                     int temp = nums[j+1];
                     nums[j+1] = nums[j];
                     nums[j] = temp;
+
+                    hasSwapped = true;
                 }
             }
+
+            // Optimization #1, if there were no swaps in the current pass, the array is sorted
+            if (!hasSwapped) {
+                break;
+            }
+
+            sortedIdx--; // Optimization #2
         }
         return nums;
     }
