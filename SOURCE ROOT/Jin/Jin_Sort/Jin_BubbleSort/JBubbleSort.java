95e3dddd380de104d39308ee3542255fe8568c84
jiny2 <jiny2019@outlook.com>
2025-06-29 13:26:17 -0700

    [Bubble Sort] Remove 2nd optimization and add 1st and 3rd optimization

--- SOURCE ROOT/Jin/Jin_Sort/Jin_BubbleSort/JBubbleSort.java
+++ SOURCE ROOT/Jin/Jin_Sort/Jin_BubbleSort/JBubbleSort.java
@@ -3,21 +3,26 @@ package Jin.Jin_Sort.Jin_BubbleSort;
 public class JBubbleSort {
     public static int[] bubbleSort(int[] arr) {
         int sortedPortion = arr.length - 1;
+        Integer latestSwap = null;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < sortedPortion; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
-                    sortedPortion -= 1;
+                    latestSwap = j + 1;
                 }
-
             }
+            if (latestSwap == null) {
+                break;
+            }
+            sortedPortion = latestSwap;
+            latestSwap = null;
         }
         return arr;
     }
     public static void main(String[] args) {
-        int[] arr = {4, 3, 5, 2, 3, 1};
+        int[] arr = {4, 3, 5, 2, 1};
         bubbleSort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
